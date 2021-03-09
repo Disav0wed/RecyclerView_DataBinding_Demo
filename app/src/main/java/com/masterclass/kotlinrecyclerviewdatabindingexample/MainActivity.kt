518844f051734f3582019fbd6c6451d3f44aa2d9
Ibrahim R Serpici <ibrahimrecepserpici@gmail.com>
2021-03-09 17:37:58 +0300

    initial project creation

---
+++ app/src/main/java/com/masterclass/kotlinrecyclerviewdatabindingexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.masterclass.kotlinrecyclerviewdatabindingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
